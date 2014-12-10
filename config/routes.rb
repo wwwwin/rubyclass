c2b4528865f1f6ee91fde31879d73ee6e849327e
“wwwwin” <newlife948@gmail.com>
2014-12-10 20:50:00 +0800

    adding landing page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'ideas#index'
-
+  get'/landing'=>'pages#landing'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
